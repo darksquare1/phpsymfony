569c822b4bd79bcc9495fa36de3dd455f300ba43
depressedkid <nekit05@list.ru>
2024-11-02 21:38:10 +1000

    ADD: get_project method to ProjectsController to retrieve a project by uuid with json

--- src/Controller/Api/ProjectsController.php
+++ src/Controller/Api/ProjectsController.php
@@ -9,13 +9,21 @@ use App\Repository\TaskRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Uid\Uuid;
 
-class ApiController extends AbstractController
+class ProjectsController extends AbstractController
 {
     #[Route('api/projects', name: 'api_projects', methods: ['GET'])]
     public function get_projects(ProjectRepository $projectRepository): JsonResponse
     {
         $projects = $projectRepository->findAll();
         return $this->json(['data' => $projects]);
+    }
+    #[Route('/api/projects/{id}', name: 'api_project', methods: ['GET'])]
+    public function get_project(string $id, ProjectRepository $projectRepository): JsonResponse
+    {
+        $uuid = Uuid::fromString($id);
+        $project = $projectRepository->find($uuid);
+        return $this->json(['data' => $project]);
     }
 }
