d829682e543048e112eefb8e19dc3bb9fe8937e3
depressedkid <nekit05@list.ru>
2024-11-03 22:23:14 +1000

    ADD: BaseApiController to avoid code duplication

--- src/Controller/Api/ProjectsController.php
+++ src/Controller/Api/ProjectsController.php
@@ -2,89 +2,44 @@
 
 namespace App\Controller\Api;
 
-
 use App\Entity\Project;
 use App\Form\ProjectFormType;
 use App\Repository\ProjectRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Uid\Uuid;
 
-class ProjectsController extends AbstractController
+
+class ProjectsController extends BaseApiController
 {
     #[Route('api/projects', name: 'api_projects', methods: ['GET'], format: 'json')]
     public function get_projects(ProjectRepository $projectRepository): JsonResponse
     {
-        $projects = $projectRepository->findAll();
-        if (!$projects) {
-            return $this->json(['data' => 'No projects found']);
-        }
-        return $this->json(['data' => $projects]);
+        return $this->handleGetAll($projectRepository);
     }
 
     #[Route('/api/projects/{id}', name: 'api_project', methods: ['GET'], format: 'json')]
     public function get_project(string $id, ProjectRepository $projectRepository): JsonResponse
     {
-        $uuid = Uuid::fromString($id);
-        $project = $projectRepository->find($uuid);
-        if (!$project) {
-            return $this->json(['data' => ['id' => 'Not found']], 404);
-        }
-        return $this->json(['data' => $project]);
+        return $this->handleGet($id, $projectRepository);
     }
 
     #[Route('api/add/project', name: 'api_add_project', methods: ['POST'], format: 'json')]
     public function add_project(Request $request, EntityManagerInterface $em): JsonResponse
     {
-        $project = new Project();
-        $data = json_decode($request->getContent(), true);
-        $form = $this->CreateForm(ProjectFormType::class, $project);
-        $form->submit($data);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em->persist($project);
-            $em->flush();
-            return $this->json(['data' => $project]);
-        } else {
-            $errors_list = $form->getConfig()->getType()->getInnerType()->customGetErrors($form);
-            return $this->json(['data' => $errors_list], 400);
-        }
+        return $this->handleAdd($request, $em, ProjectFormType::class, new Project());
     }
 
     #[Route('api/update/project/{id}', name: 'api_update_project', methods: ['PATCH'], format: 'json')]
     public function update_project(string $id, Request $request, EntityManagerInterface $em, ProjectRepository $projectRepository): JsonResponse
     {
-        $uuid = Uuid::fromString($id);
-        $project = $projectRepository->find($uuid);
-        $data = json_decode($request->getContent(), true);
-        $form = $this->CreateForm(ProjectFormType::class, $project);
-        $form->submit($data);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $project->updateTimestamps();
-            $em->persist($project);
-            $em->flush();
-            return $this->json(['data' => $project]);
-        } else {
-            $errors_list = $form->getConfig()->getType()->getInnerType()->customGetErrors($form);
-            return $this->json(['data' => $errors_list], 400);
-        }
+        return $this->handleUpdate($id, $request, $em, ProjectFormType::class, $projectRepository);
     }
 
     #[Route('api/delete/project/{id}', name: 'api_delete_project', methods: ['DELETE'], format: 'json')]
     public function delete_project(string $id, ProjectRepository $projectRepository, EntityManagerInterface $em): JsonResponse
     {
-        $uuid = Uuid::fromString($id);
-        $project = $projectRepository->find($uuid);
-        if (!$project) {
-            $em->remove($project);
-            $em->flush();
-            $response = new JsonResponse();
-            $response->setStatusCode(Response::HTTP_NO_CONTENT);
-            return $response;
-        }
-        return $this->json(['data' => ['id' => 'Not found']], 404);
+        return $this->handleDelete($id, $em, $projectRepository);
     }
 }
--- src/Controller/Api/ProjectsGroupController.php
+++ src/Controller/Api/ProjectsGroupController.php
@@ -2,89 +2,43 @@
 
 namespace App\Controller\Api;
 
-
 use App\Entity\ProjectsGroup;
 use App\Form\ProjectsGroupFormType;
 use App\Repository\ProjectsGroupRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Uid\Uuid;
 
-class ProjectsGroupController extends AbstractController
+class ProjectsGroupController extends BaseApiController
 {
     #[Route('api/project-groups', name: 'api_project_groups', methods: ['GET'], format: 'json')]
     public function get_project_groups(ProjectsGroupRepository $projectGroupRepository): JsonResponse
     {
-        $project_groups = $projectGroupRepository->findAll();
-        if (!$project_groups) {
-            return $this->json(['data' => 'No projects found']);
-        }
-        return $this->json(['data' => $project_groups]);
+        return $this->handleGetAll($projectGroupRepository);
     }
 
     #[Route('/api/project-groups/{id}', name: 'api_project_group', methods: ['GET'], format: 'json')]
     public function get_project_group(string $id, ProjectsGroupRepository $projectGroupRepository): JsonResponse
     {
-        $uuid = Uuid::fromString($id);
-        $project_group = $projectGroupRepository->find($uuid);
-        if (!$project_group) {
-            return $this->json(['data' => ['id' => 'Not found']], 404);
-        }
-        return $this->json(['data' => $project_group]);
+        return $this->handleGet($id, $projectGroupRepository);
     }
 
     #[Route('api/add/project-group', name: 'api_add_project_group', methods: ['POST'], format: 'json')]
     public function add_project_group(Request $request, EntityManagerInterface $em): JsonResponse
     {
-        $project_group = new ProjectsGroup();
-        $data = json_decode($request->getContent(), true);
-        $form = $this->CreateForm(ProjectsGroupFormType::class, $project_group);
-        $form->submit($data);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em->persist($project_group);
-            $em->flush();
-            return $this->json(['data' => $project_group]);
-        } else {
-            $errors_list = $form->getConfig()->getType()->getInnerType()->customGetErrors($form);
-            return $this->json(['data' => $errors_list], 400);
-        }
+        return $this->handleAdd($request, $em, ProjectsGroupFormType::class, new ProjectsGroup());
     }
 
     #[Route('api/update/project-group/{id}', name: 'api_update_project_group', methods: ['PATCH'], format: 'json')]
-    public function update_project(string $id, Request $request, EntityManagerInterface $em, ProjectsGroupRepository $projectGroupRepository): JsonResponse
+    public function update_project_group(string $id, Request $request, EntityManagerInterface $em, ProjectsGroupRepository $projectGroupRepository): JsonResponse
     {
-        $uuid = Uuid::fromString($id);
-        $project_group = $projectGroupRepository->find($uuid);
-        $data = json_decode($request->getContent(), true);
-        $form = $this->CreateForm(ProjectsGroupFormType::class, $project_group);
-        $form->submit($data);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $project_group->updateTimestamps();
-            $em->persist($project_group);
-            $em->flush();
-            return $this->json(['data' => $project_group]);
-        } else {
-            $errors_list = $form->getConfig()->getType()->getInnerType()->customGetErrors($form);
-            return $this->json(['data' => $errors_list], 400);
-        }
+        return $this->handleUpdate($id, $request, $em, ProjectsGroupFormType::class, $projectGroupRepository);
     }
 
     #[Route('api/delete/project-group/{id}', name: 'api_delete_project_group', methods: ['DELETE'], format: 'json')]
-    public function delete_project(string $id, ProjectsGroupRepository $projectRepository, EntityManagerInterface $em): JsonResponse
+    public function delete_project_group(string $id, ProjectsGroupRepository $projectRepository, EntityManagerInterface $em): JsonResponse
     {
-        $uuid = Uuid::fromString($id);
-        $project_group = $projectRepository->find($uuid);
-        if (!$project_group) {
-            $em->remove($project_group);
-            $em->flush();
-            $response = new JsonResponse();
-            $response->setStatusCode(Response::HTTP_NO_CONTENT);
-            return $response;
-        }
-        return $this->json(['data' => ['id' => 'Not found']], 404);
+        return $this->handleDelete($id, $em, $projectRepository);
     }
 }
