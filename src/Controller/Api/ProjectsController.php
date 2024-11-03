38ab1299cd432a8470c509141b45e0f27a282b86
depressedkid <nekit05@list.ru>
2024-11-03 19:00:39 +1000

    ADD: add_project method to ProjectsController and ProjectFormType

--- src/Controller/Api/ProjectsController.php
+++ src/Controller/Api/ProjectsController.php
@@ -3,27 +3,59 @@
 namespace App\Controller\Api;
 
 
+use App\Entity\Project;
+use App\Form\ProjectFormType;
 use App\Repository\ProjectRepository;
-use App\Repository\ProjectsGroupRepository;
-use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Uid\Uuid;
 
 class ProjectsController extends AbstractController
 {
-    #[Route('api/projects', name: 'api_projects', methods: ['GET'])]
+    #[Route('api/projects', name: 'api_projects', methods: ['GET'], format: 'json')]
     public function get_projects(ProjectRepository $projectRepository): JsonResponse
     {
         $projects = $projectRepository->findAll();
         return $this->json(['data' => $projects]);
     }
-    #[Route('/api/projects/{id}', name: 'api_project', methods: ['GET'])]
+
+    #[Route('/api/projects/{id}', name: 'api_project', methods: ['GET'], format: 'json')]
     public function get_project(string $id, ProjectRepository $projectRepository): JsonResponse
     {
         $uuid = Uuid::fromString($id);
         $project = $projectRepository->find($uuid);
         return $this->json(['data' => $project]);
+    }
+
+    #[Route('api/add/project', name: 'api_add_project', methods: ['POST'], format: 'json')]
+    public function add_project(Request $request, EntityManagerInterface $em): JsonResponse
+    {
+        $project = new Project();
+        $data = json_decode($request->getContent(), true);
+        $form = $this->CreateForm(ProjectFormType::class, $project);
+        $form->submit($data);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $project->updateTimestamps();
+            $em->persist($project);
+            $em->flush();
+            return $this->json(['data' => $project]);
+        } else {
+            $errors_list = [];
+            foreach ($form->getErrors() as $error) {
+                $errors_list[$form->getName()][] = $error->getMessage();
+            }
+            foreach ($form as $child) {
+                if (!$child->isValid()) {
+                    foreach ($child->getErrors() as $error) {
+                        $errors_list[$child->getName()][] = $error->getMessage();
+                    }
+                }
+            }
+
+            return $this->json(['data' => $errors_list], 400);
+        }
     }
 }
---
+++ src/Form/ProjectFormType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Project;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotNull;
+
+class ProjectFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class,[
+                'constraints' => [
+                    new NotNull(),
+                    new Length(['max' => 255, 'min' => 3])
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Project::class,
+            'csrf_protection' => false
+        ]);
+    }
+}
