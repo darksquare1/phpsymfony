1dd181df90df181954a27cb97237fc91c6a656bc
depressedkid <nekit05@list.ru>
2024-12-03 22:25:50 +1000

    ADD: more certain types for addFieldsWithConstraints method

--- src/Form/BaseFormType.php
+++ src/Form/BaseFormType.php
@@ -5,9 +5,14 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraint;
 
 abstract class BaseFormType extends AbstractType
 {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array<string, array<Constraint>> $fieldsWithConstraints
+     */
     protected function addFieldsWithConstraints(FormBuilderInterface $builder, array $fieldsWithConstraints): void
     {
         foreach ($fieldsWithConstraints as $fieldName => $constraints) {
