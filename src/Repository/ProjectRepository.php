190a06066e09afafce87f0c387af1c2311eb2b0b
depressedkid <nekit05@list.ru>
2024-12-12 20:34:07 +1000

    FIX: added methods to Repositories to show related data

--- src/Repository/ProjectRepository.php
+++ src/Repository/ProjectRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Project;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Uid\UuidV7;
 
 /**
  * @extends ServiceEntityRepository<Project>
@@ -30,4 +31,60 @@ class ProjectRepository extends ServiceEntityRepository
             ->getResult();
     }
 
+    public function findAllWithRelations(): array
+    {
+        $projects = $this->findAll();
+        $data = [];
+        foreach ($projects as $project) {
+            $tasks = $project->getTasks();
+            $taskData = [];
+            foreach ($tasks as $task) {
+                $singleTaskInfo = [
+                    'id' => $task->getId(),
+                    'name' => $task->getName(),
+                    'description' => $task->getDescription(),
+                    'updated_at' => $task->getUpdatedAt(),
+                    'created_at' => $task->getCreatedAt(),
+                ];
+                $taskData[] = $singleTaskInfo;
+            }
+            $data[] = [
+                'id' => $project->getId(),
+                'name' => $project->getName(),
+                'createdAt' => $project->getCreatedAt(),
+                'updatedAt' => $project->getUpdatedAt(),
+                'tasks' => $taskData
+
+            ];
+        }
+
+        return $data;
+    }
+
+    public function findOneByIdWithRelations(UuidV7 $id): array
+    {
+        $project = $this->find($id);
+        $tasks = $project->getTasks();
+        $taskData = [];
+        foreach ($tasks as $task) {
+            $singleTaskInfo = [
+                'id' => $task->getId(),
+                'name' => $task->getName(),
+                'description' => $task->getDescription(),
+                'updated_at' => $task->getUpdatedAt(),
+                'created_at' => $task->getCreatedAt(),
+            ];
+            $taskData[] = $singleTaskInfo;
+        }
+
+        $data[] = [
+            'id' => $project->getId(),
+            'name' => $project->getName(),
+            'createdAt' => $project->getCreatedAt(),
+            'updatedAt' => $project->getUpdatedAt(),
+            'tasks' => $taskData
+        ];
+
+        return $data;
+    }
 }
--- src/Repository/TaskRepository.php
+++ src/Repository/TaskRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Task;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Uid\Uuid;
 
 /**
  * @extends ServiceEntityRepository<Task>
@@ -21,4 +22,34 @@ class TaskRepository extends ServiceEntityRepository
         parent::__construct($registry, Task::class);
     }
 
+    public function findAllWithRelations(): array
+    {
+        $tasks = $this->findAll();
+        $data = [];
+        foreach ($tasks as $task) {
+            $data[] = [
+                'id' => $task->getId(),
+                'name' => $task->getName(),
+                'description' => $task->getDescription(),
+                'createdAt' => $task->getCreatedAt(),
+                'updatedAt' => $task->getUpdatedAt(),
+            ];
+        }
+
+        return $data;
+    }
+
+    public function findOneByIdWithRelations(Uuid $id): array
+    {
+        $task = $this->find($id);
+        $data[] = [
+            'id' => $task->getId(),
+            'name' => $task->getName(),
+            'description' => $task->getDescription(),
+            'createdAt' => $task->getCreatedAt(),
+            'updatedAt' => $task->getUpdatedAt(),
+        ];
+
+        return $data;
+    }
 }
