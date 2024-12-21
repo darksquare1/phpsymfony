5061a0962a3ece4453d13fc7d34a08628ba4b289
depressedkid <nekit05@list.ru>
2024-12-21 20:26:24 +1000

    FIX: TaskFixture, added dependency on ProjectFixture, now each project has many tasks

--- src/DataFixtures/TaskFixture.php
+++ src/DataFixtures/TaskFixture.php
@@ -2,23 +2,33 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Project;
 use App\Entity\Task;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class TaskFixture extends Fixture
+class TaskFixture extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        for ($i = 0; $i < 10; $i++) {
-            $task = new Task();
-            $task->setName('Task ' . $i);
-            $task->setDescription('Task description ' . $i);
-            $project = $this->getReference(ProjectFixture::PROJECT_REFERENCE_PREFIX . $i);
-            $project->addTask($task);
-            $manager->persist($task);
+        $projects = $manager->getRepository(Project::class)->findAll();
+
+        foreach ($projects as $project) {
+            for ($i = 0; $i < rand(5, 10); $i++) {
+                $task = new Task();
+                $task->setName('Task ' . $i);
+                $task->setDescription('Task description ' . $i);
+                $task->setProject($project);
+                $manager->persist($task);
+            }
         }
 
         $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [ProjectFixture::class];
     }
 }
